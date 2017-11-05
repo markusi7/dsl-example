f1488872084dace906d81a4889451835f56e5d1c
Ivan Markusi <ivan.markusi@undabot.com>
2017-11-05 11:21:31 +0100

    Add Activity layout and app color palette

--- app/src/main/java/co/markusi/dsl/MainActivity.kt
+++ app/src/main/java/co/markusi/dsl/MainActivity.kt
@@ -1,5 +1,14 @@
 package co.markusi.dsl
 
+import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
 
-class MainActivity : AppCompatActivity()
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        findViewById<Toolbar>(R.id.toolbar).title = getString(R.string.registration_form)
+    }
+}
